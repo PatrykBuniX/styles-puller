acc68e3dab4f3d0c76fd8f5da76d5790b7910653
PatrykBuniX <patrykbunix@gmail.com>
2021-02-07 17:06:35 +0100

    Initial commit.

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import styles from "./App.module.scss";
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className="App-header">woo</header>
+    </div>
+  );
+}
+
+export default App;
