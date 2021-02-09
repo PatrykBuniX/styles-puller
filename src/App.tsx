af54bb2f3b8a087aea9b8141423347762c5b1be5
PatrykBuniX <patrykbunix@gmail.com>
2021-02-09 21:02:19 +0100

    Create Editor component which uses AceEditor.

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,62 @@
 import styles from "./App.module.scss";
+import { useState } from "react";
+import beautify from "js-beautify";
+import { htmlStringToScss } from "src/lib";
 import { Editor } from "./components/editor/Editor";
-
-// const example = `<div class="about-intro">
-// <div class="about-content">
-//   <h2>Cool</h2>
-//   <p>It is very important.</p>
-// </div>
-// <div class="about-img-wrapper">
-//   <img loading="lazy" src="#" alt="" />
-// </div>
-// </div>`;
+const exampleCode = `<div class="wrapper"><p class="text">Hello world</p></div>`;
 
 function App() {
+  const [htmlString, setHtmlString] = useState(exampleCode);
+  const [cssString, setCssString] = useState("");
+
+  const prettifyHtml = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setHtmlString(beautify.html(htmlString));
+  };
+  const prettifyCss = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setCssString(beautify["css"](cssString));
+  };
+
+  const handleConvertClick = (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    const css = htmlStringToScss(htmlString);
+    setCssString(beautify.css(css));
+  };
+
   return (
     <div className={styles.App}>
-      <Editor />
+      <div className="htmlEditor">
+        <Editor
+          value={htmlString}
+          setValue={setHtmlString}
+          mode={"html"}
+          theme={"cobalt"}
+          name={"html-string-editor"}
+        />
+        <button onClick={prettifyHtml}>prettify</button>
+        <button>copy</button>
+      </div>
+      <div className="convertWrapper">
+        <button onClick={handleConvertClick}>convert</button>
+      </div>
+      <div className="cssEditor">
+        <Editor
+          value={cssString}
+          setValue={setCssString}
+          mode={"scss"}
+          theme={"cobalt"}
+          name={"css-editor"}
+        />
+        <button onClick={prettifyCss}>prettify</button>
+        <button onClick={prettifyCss}>copy</button>
+      </div>
     </div>
   );
+  // return (
+  //   <div className={styles.App}>
+  //     <Editor />
+  //   </div>
+  // );
 }
 
 export default App;
--- src/components/editor/Editor.tsx
+++ src/components/editor/Editor.tsx
@@ -1,58 +1,25 @@
-// import { useState } from "react";
 import AceEditor from "react-ace";
-// import "ace-builds/src-noconflict/mode-javascript";
-// import "ace-builds/src-noconflict/theme-github";
 import "ace-builds/webpack-resolver";
-import { useState } from "react";
-import beautify from "js-beautify";
-import { htmlStringToScss } from "src/lib";
-const exampleCode = `<div class="wrapper"><p class="text">Hello world</p></div>`;
 
-export const Editor = () => {
-  const [htmlString, setHtmlString] = useState(exampleCode);
-  const [cssString, setCssString] = useState("");
+interface Props {
+  setValue: any;
+  value: any;
+  mode: string;
+  theme: string;
+  name: string;
+}
 
-  const prettifyHtml = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setHtmlString(beautify.html(htmlString));
-  };
-  const prettifyCss = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    setCssString(beautify["css"](cssString));
-  };
-
-  const handleConvertClick = (
-    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-  ) => {
-    const css = htmlStringToScss(htmlString);
-    setCssString(beautify.css(css));
-  };
-
+export const Editor = ({ setValue, value, mode, theme, name }: Props) => {
   return (
-    <div className="wrapper">
-      <AceEditor
-        mode="html"
-        theme="github"
-        onChange={(val: string) => {
-          setHtmlString(val);
-        }}
-        name="html-string-editor"
-        editorProps={{ $blockScrolling: true }}
-        value={htmlString}
-        height={"250px"}
-      />
-      <button onClick={prettifyHtml}>prettify</button>
-      <button onClick={handleConvertClick}>convert</button>
-      <AceEditor
-        mode="scss"
-        theme="github"
-        onChange={(val: string) => {
-          setCssString(val);
-        }}
-        name="css-editor"
-        editorProps={{ $blockScrolling: true }}
-        value={cssString}
-        height={"250px"}
-      />
-      <button onClick={prettifyCss}>prettify</button>
-    </div>
+    <AceEditor
+      mode={mode}
+      theme={theme}
+      onChange={(val: string) => {
+        setValue(val);
+      }}
+      name={name}
+      editorProps={{ $blockScrolling: true }}
+      value={value}
+    />
   );
 };
