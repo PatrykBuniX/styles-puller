2f20cb93b57c633d2f2f5692ad0d020cc9920088
PatrykBuniX <patrykbunix@gmail.com>
2021-03-14 18:16:31 +0100

    Add multiple class support to html to object converting function.

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -14,19 +14,25 @@ type SelectorsObject = { [k: string]: any };
 
 const htmlToObject = (htmlString: string): SelectorsObject => {
   function addSelectors(nodes: NodeListOf<ChildNode>): SelectorsObject {
-    return Array.from(nodes).reduce((acc: SelectorsObject, node: ChildNode) => {
-      if (!isHTMLElement(node)) {
+    return Array.from(nodes).reduce(
+      (acc: SelectorsObject, node: ChildNode, i) => {
+        if (!isHTMLElement(node)) {
+          return acc;
+        }
+        const { classList, tagName } = node;
+
+        const nextField = classList.value
+          ? `${classList.value
+              .split(" ")
+              .map((c) => `.${c}`)
+              .join("")}`
+          : tagName.toLowerCase();
+
+        acc[nextField] = addSelectors(node.childNodes);
         return acc;
-      }
-      const { classList, tagName } = node as HTMLElement;
-
-      const nextField = classList.value
-        ? `.${classList.value}`
-        : tagName.toLowerCase();
-
-      acc[nextField] = addSelectors(node.childNodes);
-      return acc;
-    }, {});
+      },
+      {}
+    );
   }
   const nodeElement = createElementFromHTML(htmlString);
   const selectorsObject = addSelectors(nodeElement);
@@ -34,16 +40,27 @@ const htmlToObject = (htmlString: string): SelectorsObject => {
 };
 
 const selectorsObjectToScss = (selectorsObject: SelectorsObject): string => {
-  const jsonString = JSON.stringify(selectorsObject);
-  const formatted = /{(.+)}/g
-    .exec(jsonString)![1]
-    .replace(/"([^"]+)":/g, "$1")
-    .replace(/\,/g, "\n");
-  return formatted;
+  console.log(selectorsObject);
+
+  const getSelectors = (object: any) => {
+    let currentElement = "";
+    Object.keys(object).forEach((key) => {
+      const keys = key.split(".");
+      keys.shift();
+      if (keys.length > 1) {
+        const childSelectors = getSelectors(object[key]);
+        currentElement += `.${keys[0]}{&.${keys[1]}{${childSelectors}}}`;
+      } else {
+        const childSelectors = getSelectors(object[key]);
+        currentElement += key + "{" + childSelectors + "}";
+      }
+    });
+    return currentElement;
+  };
+  return getSelectors(selectorsObject);
 };
 
 export const htmlStringToScss = (htmlString: string): string => {
-  console.log(htmlString);
   const selectorsObject = htmlToObject(htmlString);
   return selectorsObjectToScss(selectorsObject);
 };
