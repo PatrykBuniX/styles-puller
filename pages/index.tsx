68b8e8f31e22787ac89c5125630d6137be451635
PatrykBuniX <patrykbunix@gmail.com>
2021-05-15 15:44:14 +0200

    Handle ctr+s (save) event (save website as...) window won't be displayed.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,21 @@
 import Head from "next/head";
+import { useEffect } from "react";
 import { Main } from "../components/Main/Main";
 
+const handleCtrSave = (e: KeyboardEvent) => {
+  if (
+    (window.navigator.platform.match("Mac") ? e.metaKey : e.ctrlKey) &&
+    e.key === "s"
+  ) {
+    e.preventDefault();
+  }
+};
+
 function App() {
+  useEffect(() => {
+    window.addEventListener("keydown", handleCtrSave, false);
+    return () => window.removeEventListener("keydown", handleCtrSave, false);
+  }, []);
   return (
     <>
       <Head>
