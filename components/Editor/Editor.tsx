58aa9249606f57fad22a4d49f89599b3a28724da
PatrykBuniX <patrykbunix@gmail.com>
2021-05-22 18:10:42 +0200

    Add displayName on memoized Editor component to make its name visible in dev tools.

--- components/Editor/Editor.tsx
+++ components/Editor/Editor.tsx
@@ -13,66 +13,68 @@ interface Props {
   isDarkTheme: boolean;
 }
 
-export const Editor = React.memo(
-  ({ value, updateEditorValue, lang, mode, isDarkTheme }: Props) => {
-    const [isCopiedActive, setIsCopiedActive] = useState(false);
-    const [editorHeight, setEditorHeight] = useState(700);
+const Editor = React.memo(({ value, updateEditorValue, lang, mode, isDarkTheme }: Props) => {
+  const [isCopiedActive, setIsCopiedActive] = useState(false);
+  const [editorHeight, setEditorHeight] = useState(700);
 
-    useEffect(() => {
+  useEffect(() => {
+    setEditorHeight(window.innerWidth <= 1100 ? 500 : 700);
+    const handleWindowResize = () => {
       setEditorHeight(window.innerWidth <= 1100 ? 500 : 700);
-      const handleWindowResize = () => {
-        setEditorHeight(window.innerWidth <= 1100 ? 500 : 700);
-      };
-      window.addEventListener("resize", handleWindowResize);
-
-      return () => window.removeEventListener("resize", handleWindowResize);
-    }, []);
-
-    const prettify = () => {
-      updateEditorValue(beautify[lang](value));
     };
+    window.addEventListener("resize", handleWindowResize);
 
-    const handleCopyClick = () => {
-      navigator.clipboard.writeText(value).then(() => {
-        if (isCopiedActive) return;
-        setIsCopiedActive(true);
-        setTimeout(() => {
-          setIsCopiedActive(false);
-        }, 500);
-      });
-    };
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, []);
 
-    const handleEditorChange = (value: string | undefined) => {
-      value && updateEditorValue(value);
-    };
+  const prettify = () => {
+    updateEditorValue(beautify[lang](value));
+  };
 
-    return (
-      <div className={styles.codeEditor}>
-        <h2>
-          Your <strong>{mode}</strong> code:
-        </h2>
-        <MonacoEditor
-          height={editorHeight}
-          value={value}
-          theme={isDarkTheme ? "vs-dark" : "light"}
-          onChange={handleEditorChange}
-          language={mode}
-          loading={<Loading />}
-          options={{
-            minimap: { enabled: false },
-            padding: { top: 20 },
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(value).then(() => {
+      if (isCopiedActive) return;
+      setIsCopiedActive(true);
+      setTimeout(() => {
+        setIsCopiedActive(false);
+      }, 500);
+    });
+  };
+
+  const handleEditorChange = (value: string | undefined) => {
+    value && updateEditorValue(value);
+  };
+
+  return (
+    <div className={styles.codeEditor}>
+      <h2>
+        Your <strong>{mode}</strong> code:
+      </h2>
+      <MonacoEditor
+        height={editorHeight}
+        value={value}
+        theme={isDarkTheme ? "vs-dark" : "light"}
+        onChange={handleEditorChange}
+        language={mode}
+        loading={<Loading />}
+        options={{
+          minimap: { enabled: false },
+          padding: { top: 20 },
+        }}
+      />
+      <div className={styles.buttonsWrapper}>
+        <button onClick={prettify}>prettify</button>
+        <button
+          onClick={handleCopyClick}
+          dangerouslySetInnerHTML={{
+            __html: isCopiedActive ? "copied&nbsp;&check;" : "copy",
           }}
-        />
-        <div className={styles.buttonsWrapper}>
-          <button onClick={prettify}>prettify</button>
-          <button
-            onClick={handleCopyClick}
-            dangerouslySetInnerHTML={{
-              __html: isCopiedActive ? "copied&nbsp;&check;" : "copy",
-            }}
-          ></button>
-        </div>
+        ></button>
       </div>
-    );
-  }
-);
+    </div>
+  );
+});
+
+Editor.displayName = "Editor";
+
+export { Editor };
