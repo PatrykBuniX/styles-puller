3652324924163b5fe5321a2dfb019c9f13fbe73e
PatrykBuniX <patrykbunix@gmail.com>
2021-06-03 18:31:59 +0200

    Move svg icons to separate components.

--- components/ToggleThemeButton/ToggleThemeButton.tsx
+++ components/ToggleThemeButton/ToggleThemeButton.tsx
@@ -1,26 +1,8 @@
+import { MoonIcon } from "../icons/MoonIcon";
+import { SunIcon } from "../icons/SunIcon";
 import styles from "./ToggleThemeButton.module.scss";
 
 type Props = { isDarkTheme: boolean; handleToggleTheme: () => void };
-
-const sunIcon = (
-  <svg width="32" height="32" viewBox="0 0 129 129" xmlns="http://www.w3.org/2000/svg">
-    <path d="M64.5 92.6c15.5 0 28-12.6 28-28s-12.6-28-28-28-28 12.6-28 28 12.5 28 28 28zm0-47.9c11 0 19.9 8.9 19.9 19.9 0 11-8.9 19.9-19.9 19.9s-19.9-8.9-19.9-19.9c0-11 8.9-19.9 19.9-19.9zM68.6 23.6V10.7c0-2.3-1.8-4.1-4.1-4.1s-4.1 1.8-4.1 4.1v12.9c0 2.3 1.8 4.1 4.1 4.1s4.1-1.8 4.1-4.1zM60.4 105.6v12.9c0 2.3 1.8 4.1 4.1 4.1s4.1-1.8 4.1-4.1v-12.9c0-2.3-1.8-4.1-4.1-4.1s-4.1 1.8-4.1 4.1zM96.4 38.5l9.1-9.1c1.6-1.6 1.6-4.2 0-5.8-1.6-1.6-4.2-1.6-5.8 0l-9.1 9.1c-1.6 1.6-1.6 4.2 0 5.8.8.8 1.8 1.2 2.9 1.2s2.1-.4 2.9-1.2zM23.5 105.6c.8.8 1.8 1.2 2.9 1.2 1 0 2.1-.4 2.9-1.2l9.1-9.1c1.6-1.6 1.6-4.2 0-5.8-1.6-1.6-4.2-1.6-5.8 0l-9.1 9.1c-1.6 1.6-1.6 4.2 0 5.8zM122.5 64.6c0-2.3-1.8-4.1-4.1-4.1h-12.9c-2.3 0-4.1 1.8-4.1 4.1s1.8 4.1 4.1 4.1h12.9c2.2 0 4.1-1.8 4.1-4.1zM10.6 68.7h12.9c2.3 0 4.1-1.8 4.1-4.1s-1.8-4.1-4.1-4.1H10.6c-2.3 0-4.1 1.8-4.1 4.1s1.9 4.1 4.1 4.1zM102.6 106.8c1 0 2.1-.4 2.9-1.2 1.6-1.6 1.6-4.2 0-5.8l-9.1-9.1c-1.6-1.6-4.2-1.6-5.8 0-1.6 1.6-1.6 4.2 0 5.8l9.1 9.1c.8.8 1.9 1.2 2.9 1.2zM38.4 38.5c1.6-1.6 1.6-4.2 0-5.8l-9.1-9.1c-1.6-1.6-4.2-1.6-5.8 0-1.6 1.6-1.6 4.2 0 5.8l9.1 9.1c.8.8 1.8 1.2 2.9 1.2s2.1-.4 2.9-1.2z" />
-  </svg>
-);
-
-const moonIcon = (
-  <svg
-    width="32"
-    height="32"
-    viewBox="0 0 32 32"
-    fill="none"
-    stroke="#000"
-    strokeWidth="2"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    <path d="M14 2C9 2 3 7 3 15s6 14 14 14 13-6 13-11C19 25 7 13 14 2z" />
-  </svg>
-);
 
 export const ToggleThemeButton = ({ isDarkTheme, handleToggleTheme }: Props) => {
   return (
@@ -29,7 +11,7 @@ export const ToggleThemeButton = ({ isDarkTheme, handleToggleTheme }: Props) => 
       className={styles.toggleThemeButton}
       onClick={handleToggleTheme}
     >
-      {isDarkTheme ? sunIcon : moonIcon}
+      {isDarkTheme ? <SunIcon /> : <MoonIcon />}
     </button>
   );
 };
---
+++ components/icons/MoonIcon.tsx
@@ -0,0 +1,15 @@
+export const MoonIcon = () => {
+  return (
+    <svg
+      width="32"
+      height="32"
+      viewBox="0 0 32 32"
+      fill="none"
+      stroke="#000"
+      strokeWidth="2"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M14 2C9 2 3 7 3 15s6 14 14 14 13-6 13-11C19 25 7 13 14 2z" />
+    </svg>
+  );
+};
